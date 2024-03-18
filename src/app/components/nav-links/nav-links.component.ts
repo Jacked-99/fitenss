270bde336500d15ffa0c04b0402cefbf7552029c
Jacked <jaceb134@gmail.com>
2024-03-18 09:57:12 +0100

    changed theme colors

--- src/app/components/nav-links/nav-links.component.ts
+++ src/app/components/nav-links/nav-links.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-nav-links',
   standalone: true,
-  imports: [],
+  imports: [MatMenuModule, MatButtonModule, RouterModule, MatCardModule],
   templateUrl: './nav-links.component.html',
-  styleUrl: './nav-links.component.scss'
+  styleUrl: './nav-links.component.scss',
 })
-export class NavLinksComponent {
-
-}
+export class NavLinksComponent {}
