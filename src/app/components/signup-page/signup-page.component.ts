8f347a06732ed430046af0519bb1b62d38dd83b7
Jacked <jaceb134@gmail.com>
2024-03-19 10:29:25 +0100

    signup page content

--- src/app/components/signup-page/signup-page.component.ts
+++ src/app/components/signup-page/signup-page.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { AuthPageTempComponent } from '../auth-page-temp/auth-page-temp.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-signup-page',
   standalone: true,
-  imports: [AuthPageTempComponent],
+  imports: [AuthPageTempComponent, MatIconModule, MatButtonModule],
   templateUrl: './signup-page.component.html',
   styleUrl: './signup-page.component.scss',
 })
