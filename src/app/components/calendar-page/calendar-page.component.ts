05e8e1aa9057c1dc2a7703b01f65b3e9c9065e38
Jacked <jaceb134@gmail.com>
2024-03-19 09:44:09 +0100

    used template in pages components

--- src/app/components/calendar-page/calendar-page.component.ts
+++ src/app/components/calendar-page/calendar-page.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { ContentPageTempComponent } from '../content-page-temp/content-page-temp.component';
 
 @Component({
   selector: 'app-calendar-page',
   standalone: true,
-  imports: [],
+  imports: [ContentPageTempComponent],
   templateUrl: './calendar-page.component.html',
-  styleUrl: './calendar-page.component.scss'
+  styleUrl: './calendar-page.component.scss',
 })
-export class CalendarPageComponent {
-
-}
+export class CalendarPageComponent {}
--- src/app/components/product-base-page/product-base-page.component.ts
+++ src/app/components/product-base-page/product-base-page.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { ContentPageTempComponent } from '../content-page-temp/content-page-temp.component';
 
 @Component({
   selector: 'app-product-base-page',
   standalone: true,
-  imports: [],
+  imports: [ContentPageTempComponent],
   templateUrl: './product-base-page.component.html',
-  styleUrl: './product-base-page.component.scss'
+  styleUrl: './product-base-page.component.scss',
 })
-export class ProductBasePageComponent {
-
-}
+export class ProductBasePageComponent {}
