8f23433704e1f6eb38ca321e1a25aaf3324191f5
Jacked <jaceb134@gmail.com>
2024-03-21 09:17:30 +0100

    placeholder products array

--- src/app/components/product-base-page/product-base-page.component.ts
+++ src/app/components/product-base-page/product-base-page.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ContentPageTempComponent } from '../content-page-temp/content-page-temp.component';
 import { ProductItemComponent } from '../product-item/product-item.component';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { Product } from '../../shared/product';
 
 @Component({
   selector: 'app-product-base-page',
@@ -10,4 +11,37 @@ import { MatExpansionModule } from '@angular/material/expansion';
   templateUrl: './product-base-page.component.html',
   styleUrl: './product-base-page.component.scss',
 })
-export class ProductBasePageComponent {}
+export class ProductBasePageComponent {
+  fakeProducts: Product[] = [
+    {
+      id: '11',
+      name: 'oats',
+      imgSrc:
+        'https://www.hsph.harvard.edu/nutritionsource/wp-content/uploads/sites/30/2018/03/oats-701299_1920.jpg',
+      desc: 'Healthy carbs',
+      data: {
+        calories: 360,
+        carbs: 55,
+        fiber: 11,
+        protein: 14,
+        fat: 8,
+        sugar: 1,
+      },
+    },
+    {
+      id: '12',
+      name: 'Milk',
+      imgSrc:
+        'https://cdn.britannica.com/77/200377-050-4326767F/milk-splashing-glass.jpg',
+      desc: 'From the cow',
+      data: {
+        calories: 360,
+        carbs: 55,
+        fiber: 11,
+        protein: 14,
+        fat: 8,
+        sugar: 1,
+      },
+    },
+  ];
+}
--- src/app/shared/product.ts
+++ src/app/shared/product.ts
@@ -1,12 +1,14 @@
 export interface Product {
   id: string;
   name: string;
+  imgSrc: string;
   data: {
     calories: number;
     protein: number;
     fat: number;
     carbs: number;
     sugar: number;
+    fiber: number;
   };
   desc: string;
 }
