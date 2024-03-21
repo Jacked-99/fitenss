bd746d6df659136d7a588de2c27b57b0de06c946
Jacked <jaceb134@gmail.com>
2024-03-21 09:59:43 +0100

    display nutritional info

--- src/app/components/product-item/product-item.component.ts
+++ src/app/components/product-item/product-item.component.ts
@@ -1,13 +1,14 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { Product } from '../../shared/product';
 import { TitleCasePipe } from '@angular/common';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-product-item',
   standalone: true,
-  imports: [MatListModule, MatExpansionModule, TitleCasePipe],
+  imports: [MatListModule, MatExpansionModule, TitleCasePipe, MatDividerModule],
   templateUrl: './product-item.component.html',
   styleUrl: './product-item.component.scss',
 })
