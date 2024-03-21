9ee9886873847e73a1e8580e99e33e57c43b4af9
Jacked <jaceb134@gmail.com>
2024-03-21 09:25:58 +0100

    added pipes and send data to product item component

--- src/app/components/product-item/product-item.component.ts
+++ src/app/components/product-item/product-item.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { Product } from '../../shared/product';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-product-item',
   standalone: true,
-  imports: [MatListModule, MatExpansionModule],
+  imports: [MatListModule, MatExpansionModule, TitleCasePipe],
   templateUrl: './product-item.component.html',
   styleUrl: './product-item.component.scss',
 })
-export class ProductItemComponent {}
+export class ProductItemComponent {
+  @Input() productData?: Product;
+}
