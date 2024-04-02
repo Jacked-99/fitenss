888b438b25bd0074a465ab43240a148dfc41041d
Jacked <jaceb134@gmail.com>
2024-04-02 14:05:16 +0200

    chart component, placeholder data

---
+++ src/app/components/chart/chart.component.ts
@@ -0,0 +1,36 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
+
+@Component({
+  selector: 'app-chart',
+  standalone: true,
+  imports: [],
+  templateUrl: './chart.component.html',
+  styleUrl: './chart.component.scss',
+})
+export class ChartComponent implements OnInit {
+  @Input() labels: string[] = [];
+  @Input() dataPoints: number[] = [];
+  chart: any;
+  onCreateChart() {
+    this.chart = new Chart('chartComp', {
+      type: 'doughnut',
+      data: {
+        labels: this.labels,
+        datasets: [
+          {
+            data: [100, 200, 300],
+            backgroundColor: [
+              'rgb(255, 99, 132)',
+              'rgb(54, 162, 235)',
+              'rgb(255, 205, 86)',
+            ],
+          },
+        ],
+      },
+    });
+  }
+  ngOnInit(): void {
+    this.onCreateChart();
+  }
+}
