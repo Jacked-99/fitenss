2e6d19417130f2eb8fa269f2f066732310ee0ebc
Jacked <jaceb134@gmail.com>
2024-04-02 09:45:52 +0200

    dashboard dialog component

---
+++ src/app/components/dashboard-dialog/dashboard-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard-dialog.component.html',
+  styleUrl: './dashboard-dialog.component.scss'
+})
+export class DashboardDialogComponent {
+
+}
