6c5a30065a299176eb51104c17359e463184ace3
Jacked <jaceb134@gmail.com>
2024-04-03 15:33:47 +0200

    basic display of nutreints in dashboard

--- src/app/components/dashboard-page/dashboard-page.component.ts
+++ src/app/components/dashboard-page/dashboard-page.component.ts
@@ -5,6 +5,9 @@ import { ChartComponent } from '../chart/chart.component';
 import { MatButtonModule } from '@angular/material/button';
 import { IntakeService } from '../../shared/intake.service';
 import { Subscription, take } from 'rxjs';
+import { Intake } from '../../shared/intake';
+import { UpperCasePipe } from '@angular/common';
+import { KeyValuePipe } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard-page',
@@ -14,12 +17,15 @@ import { Subscription, take } from 'rxjs';
     MatDivider,
     ChartComponent,
     MatButtonModule,
+    UpperCasePipe,
+    KeyValuePipe,
   ],
   templateUrl: './dashboard-page.component.html',
   styleUrl: './dashboard-page.component.scss',
 })
 export class DashboardPageComponent implements OnInit, OnDestroy {
   intakeSub!: Subscription;
+  currentCalories: Intake[] = [];
   constructor(private intakeService: IntakeService) {}
   onDialogOpen() {
     this.intakeService.onCaloriesAdd({
@@ -31,13 +37,17 @@ export class DashboardPageComponent implements OnInit, OnDestroy {
       sugar: 5,
       fiber: 5,
     });
+    console.log(this.currentCalories);
   }
   onDialogClose() {
     this.intakeService.onCaloriesRemove('oats');
   }
+  getProductKeys() {
+    return Object.keys(this.currentCalories[0]);
+  }
   ngOnInit(): void {
-    this.intakeSub = this.intakeService.$currentIntake.subscribe((val) =>
-      console.log(val)
+    this.intakeSub = this.intakeService.$currentIntake.subscribe(
+      (val) => (this.currentCalories = [...val])
     );
   }
   ngOnDestroy(): void {
