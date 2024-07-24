74211200206b531ef75dcaeb67151d008a441ada
Jacked <jaceb134@gmail.com>
2024-07-24 10:59:52 +0200

    working month change

--- src/app/components/calendar-display/calendar-display.component.ts
+++ src/app/components/calendar-display/calendar-display.component.ts
@@ -1,5 +1,14 @@
 import { NgClass } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import {
+  trigger,
+  style,
+  animate,
+  transition,
+  query,
+  stagger,
+  state,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-calendar-display',
@@ -7,11 +16,28 @@ import { Component, Input } from '@angular/core';
   imports: [NgClass],
   templateUrl: './calendar-display.component.html',
   styleUrl: './calendar-display.component.scss',
+  animations: [
+    trigger('listAnimation', [
+      transition('* => *', [
+        query('.grid-element', [
+          style({ opacity: 0, transform: 'none' }),
+          stagger(30, [
+            animate(
+              '500ms  cubic-bezier(0.35, 0, 0.25, 1)',
+              style({ opacity: 1, transform: 'none' })
+            ),
+          ]),
+        ]),
+      ]),
+      transition('hidden => visible', [animate('0.1s')]),
+    ]),
+  ],
 })
 export class CalendarDisplayComponent {
   @Input() newDate: Date = new Date();
-  numOfDays = this.newDate.getDate();
+  numOfDays = 0;
   today = new Date().getDate();
+  month = this.newDate.getMonth() + 1;
 
   upadteArr(): number[] {
     this.numOfDays = this.newDate.getDate();
@@ -21,9 +47,10 @@ export class CalendarDisplayComponent {
   }
   checkToday() {
     let todayDate = new Date();
+    this.month = todayDate.getMonth() + 1;
     if (
       todayDate.getFullYear() == this.newDate.getFullYear() &&
-      todayDate.getMonth() == this.newDate.getMonth() + 1
+      todayDate.getMonth() + 1 == this.newDate.getMonth() + 1
     ) {
       return true;
     } else {
--- src/app/components/calendar-page/calendar-page.component.ts
+++ src/app/components/calendar-page/calendar-page.component.ts
@@ -1,32 +1,37 @@
 import { Component } from '@angular/core';
 import { ContentPageTempComponent } from '../content-page-temp/content-page-temp.component';
 import { CalendarDisplayComponent } from '../calendar-display/calendar-display.component';
+import { MatIcon } from '@angular/material/icon';
+import { MatIconButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-calendar-page',
   standalone: true,
-  imports: [ContentPageTempComponent, CalendarDisplayComponent],
+  imports: [
+    ContentPageTempComponent,
+    CalendarDisplayComponent,
+    MatIcon,
+    MatIconButton,
+  ],
   templateUrl: './calendar-page.component.html',
   styleUrl: './calendar-page.component.scss',
 })
 export class CalendarPageComponent {
-  dateVal = new Date(new Date().getFullYear(), new Date().getMonth(), 0);
+  dateVal = new Date(new Date().getFullYear(), new Date().getMonth() + 1, 0);
   monthVal = this.dateVal.getMonth() + 1;
   yearVal = this.dateVal.getFullYear();
 
   today = new Date().getDate();
 
-  addMonth() {
-    let newMonth = (this.monthVal += 1);
+  changeMonth(number = 1) {
+    let newMonth = (this.monthVal += number);
     if (this.monthVal > 12) {
       newMonth = 1;
       this.yearVal += 1;
     }
     const newDate = new Date(this.yearVal, newMonth, 0);
-    // this.monthVal = newMonth;
-    // this.days = newDate.getDate();
-    this.dateVal = newDate;
+    this.monthVal = newMonth;
 
-    console.log(this.dateVal.getDate());
+    this.dateVal = newDate;
   }
 }
