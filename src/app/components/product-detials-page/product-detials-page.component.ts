91a2205b28925153b8c9a0af41ed91e44705021e
Jacked <jaceb134@gmail.com>
2024-08-01 10:09:20 +0200

    chart display in product details

--- src/app/components/product-detials-page/product-detials-page.component.ts
+++ src/app/components/product-detials-page/product-detials-page.component.ts
@@ -3,11 +3,21 @@ import { Product } from '../../shared/product';
 import { ProductsService } from '../../shared/products.service';
 import { Subscription } from 'rxjs';
 import { MatListModule } from '@angular/material/list';
+import { ChartComponent } from '../chart/chart.component';
+import { ChartService } from '../../shared/chart.service';
+import { Intake } from '../../shared/intake';
+import { ContentPageTempComponent } from '../content-page-temp/content-page-temp.component';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-product-detials-page',
   standalone: true,
-  imports: [MatListModule],
+  imports: [
+    MatListModule,
+    ChartComponent,
+    ContentPageTempComponent,
+    TitleCasePipe,
+  ],
   templateUrl: './product-detials-page.component.html',
   styleUrl: './product-detials-page.component.scss',
 })
@@ -17,16 +27,35 @@ export class ProductDetialsPageComponent implements OnInit, OnDestroy {
   productData?: Product;
   productSub?: Subscription;
 
-  constructor(private prodeuctService: ProductsService) {}
+  constructor(
+    private prodeuctService: ProductsService,
+    private chartService: ChartService
+  ) {}
   ngOnInit(): void {
     let idVal = this.id?.slice(0, this.id?.indexOf('_'));
     if (idVal) {
       this.prodeuctService.getProduct(idVal?.toString()).subscribe({
-        next: (val) => (this.productData = val),
+        next: (val) => (
+          (this.productData = val),
+          this.chartService.setChartData(this.createIntake(this.productData!))
+        ),
       });
     }
   }
   ngOnDestroy(): void {
     this.productSub?.unsubscribe();
+  }
+  createIntake(data: Product) {
+    let intakeData: Intake[] = [];
+    let temp = {} as any;
+    if (data) {
+      let keys: keyof typeof data.nutrients;
+      for (keys in data.nutrients) {
+        temp[keys] = data.nutrients[keys];
+      }
+      temp['product'] = data.name;
+      intakeData.push(temp);
+    }
+    return intakeData;
   }
 }
