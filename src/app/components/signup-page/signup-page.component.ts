e8e5d89a8637ffadb240559695ef290619459efd
Jacked <jaceb134@gmail.com>
2024-08-07 09:35:11 +0200

    working error checking in signup form

--- src/app/components/signup-page/signup-page.component.ts
+++ src/app/components/signup-page/signup-page.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { AuthPageTempComponent } from '../auth-page-temp/auth-page-temp.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import {
+  AbstractControl,
   FormControl,
   FormGroup,
   ReactiveFormsModule,
@@ -26,6 +27,8 @@ import { MatInputModule } from '@angular/material/input';
   styleUrl: './signup-page.component.scss',
 })
 export class SignupPageComponent {
+  passwordError = signal('');
+
   signInForm = new FormGroup({
     username: new FormControl('', [
       Validators.required,
@@ -34,7 +37,25 @@ export class SignupPageComponent {
     password: new FormControl('', [
       Validators.required,
       Validators.minLength(5),
-      Validators.pattern('[A-Za-z0-9]*'),
+      Validators.pattern('^(?=.*d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$'),
     ]),
   });
+
+  get username() {
+    return this.signInForm.get('username');
+  }
+  get password() {
+    return this.signInForm.get('password');
+  }
+  checkErrors(FormControl: AbstractControl | null) {
+    if (FormControl?.hasError('required')) {
+      return 'You must enter a value';
+    } else if (FormControl?.hasError('minlength')) {
+      return 'Value must be longer than 5';
+    } else if (FormControl?.hasError('pattern')) {
+      return 'Value must contain one uppercase, lowercase, number';
+    } else {
+      return '';
+    }
+  }
 }
