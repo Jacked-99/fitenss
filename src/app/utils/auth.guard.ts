40f15740298ca6a4da693a873a539538f47c157b
Jacked <jaceb134@gmail.com>
2024-09-16 08:50:34 +0200

    removed react router import

--- src/app/utils/auth.guard.ts
+++ src/app/utils/auth.guard.ts
@@ -1,7 +1,6 @@
 import { inject } from '@angular/core';
 import { Auth } from '@angular/fire/auth';
 import { CanActivateFn } from '@angular/router';
-import { redirect } from 'react-router-dom';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const auth = inject(Auth);
